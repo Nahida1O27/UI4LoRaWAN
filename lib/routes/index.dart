1e57a3501b9eb4ea4d934f4f5570f43d277565c6
Nahida1O27 <Nahida1027@aliyun.com>
2026-03-19 14:50:43 +0800

    搭建主页路由

---
+++ lib/routes/index.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lorawan/pages/Main/index.dart';
+
+Widget getRootWidget(){
+  return MaterialApp(
+    //命名路由
+    initialRoute: "/",
+    routes: getRootRoutes(),
+  );
+}
+
+Map<String, Widget Function(BuildContext)> getRootRoutes(){
+  return {
+    '/': (context) => const MainPage(),//主页路由
+  };
+}
